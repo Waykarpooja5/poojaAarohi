2d34c4af1103be509fccde9dbae262bc4d161345
pooja <waykarpooja5@gmail.com>
2024-01-08 21:05:16 +0530

    Add project files.

---
+++ WebAPI/models/User.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace WebAPI.models
+{
+    public class User
+    {
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Contact { get; set; }
+    }
+}
---
+++ WebAPI/models/UserContact.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Writers;
+using System.Data.Common;
+using System.Reflection.PortableExecutable;
+
+namespace WebAPI.models
+{
+    public class UserContact:DbContext
+    {
+
+      public UserContact(DbContextOptions options):base(options)
+        { 
+         
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
